9eae2ae048f994a6e3bd9b199ab106f5fb7143f9
kimura <pankata79.th@gmail.com>
2020-11-14 07:13:04 +0900

    log outボタン

--- src/main/java/com/example/demo/controller/SecurityController.java
+++ src/main/java/com/example/demo/controller/SecurityController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 	
-	@GetMapping("/ogin")
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
